39ea74395cbbe9fbda24de0257e91b2142de5d0d
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-02 16:20:56 +0200

    Clarify typing and naming

--- year2025/src/days/day01.rs
+++ year2025/src/days/day01.rs
@@ -4,18 +4,18 @@ pub fn solve(input_file: &str) {
     let input: String = fs::read_to_string(input_file).unwrap();
     let by_line: Vec<(&str, i16)> = input
         .lines()
-        .map(|s| s.split_at(1))
-        .map(|t| (t.0, t.1.parse::<i16>().unwrap()))
+        .map(|s: &str| s.split_at(1))
+        .map(|t: (&str, &str)| (t.0, t.1.parse::<i16>().unwrap()))
         .collect();
 
-    let day_one_combination = solve_day_one(by_line);
-    println!("Day one combination: {}", day_one_combination);
+    let part_one_combination: u16 = solve_day_one(by_line);
+    println!("Part one combination: {}", part_one_combination);
 }
 
 fn solve_day_one(lines: Vec<(&str, i16)>) -> u16 {
 
-    let mut combination = 0;
-    let mut current_position = 50;
+    let mut combination: u16 = 0;
+    let mut current_position: i16 = 50;
 
     for line in lines {
         if line.0 == "L" {
@@ -27,6 +27,5 @@ fn solve_day_one(lines: Vec<(&str, i16)>) -> u16 {
             combination += 1;
         }
     }
-
     return combination;
 }
