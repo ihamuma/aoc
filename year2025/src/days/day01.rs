22fa698d32908a70e3b428da95948d74c186bf02
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-02 13:17:49 +0200

    Revert "Return to original verbose solution"
    This reverts commit 41445e41bd871a74b6ef34e922c2bff9ed4b6520.

--- year2025/src/days/day01.rs
+++ year2025/src/days/day01.rs
@@ -1,10 +1,12 @@
 use std::fs;
 
 pub fn solve(input_file: &str) {
-    let by_line: Vec<String> = fs::read_to_string(input_file)
+    let by_line: Vec<(&str, i16)> = fs::read_to_string(input_file)
         .unwrap()   
         .lines()
         .map(|l| String::from(l))
+        .map(|s| s.split_at(1))
+        .map(|t| (t.0, t.1.parse::<i16>().unwrap()))
         .collect();
 
     let day_one_combination = solve_day_one(by_line);
@@ -13,29 +15,21 @@ pub fn solve(input_file: &str) {
     ()
 }
 
-fn solve_day_one(lines: Vec<String>) -> u16 {
+fn solve_day_one(lines: Vec<(&str, i16)>) -> u16 {
 
     let mut combination = 0;
     let mut current_position = 50;
 
     for line in lines {
-        let interpreted_line = interpret_line(&line);
-
-        current_position += interpreted_line;
-        
+        if line.0 == "L" {
+            current_position += -line.1;
+        } else {
+            current_position += line.1;
+        }        
         if current_position % 100 == 0 {
             combination += 1;
         }
     }
 
     return combination;
-}
-
-fn interpret_line(line: &str) -> i16 {
-    let (direction, to_turn): (&str, &str) = line.split_at(1);
-    if direction == "L" {
-        return -to_turn.parse::<i16>().unwrap();
-    } else {
-        return to_turn.parse::<i16>().unwrap()
-    }
 }
