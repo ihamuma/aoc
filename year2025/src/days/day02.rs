7fc75215cca77f7a9a2c952a354631d33a679c3f
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-03 10:22:16 +0200

    Plan part 2 solution
    Need to work on my actual work now :)

--- year2025/src/days/day02.rs
+++ year2025/src/days/day02.rs
@@ -4,17 +4,20 @@ pub fn solve(input_file: & str) {
     let input_file: String = fs::read_to_string(input_file).unwrap();
     let ranges: Vec<Vec<u64>> = input_file.split(',').map(|r| r.split('-').map(|s| s.parse::<u64>().unwrap()).collect()).collect();
 
-    let part_one_total = solve_part_one(&ranges);
-    println!("Part one total: {}", part_one_total)
+    let part_one_total: u64 = solve_part_one(&ranges);
+    println!("Part one total: {}", part_one_total);
+
+    let part_two_total: u64 = solve_part_two(&ranges);
+    println!("Part two total: {}", part_one_total);
 }
 
 fn solve_part_one(id_ranges: &Vec<Vec<u64>>) -> u64 {
-    let mut total = 0;
+    let mut total: u64 = 0;
     for id_range in id_ranges {
-        let first_id = id_range[0];
-        let last_id = id_range[1];
+        let first_id: u64 = id_range[0];
+        let last_id: u64 = id_range[1];
         for id in first_id..=last_id {
-            let string_id = id.to_string();
+            let string_id: String = id.to_string();
             let (id1, id2) = string_id.split_at(string_id.len()/2);
             if id1 == id2 {
                 total += id;
@@ -22,4 +25,14 @@ fn solve_part_one(id_ranges: &Vec<Vec<u64>>) -> u64 {
         }
     }
     return total;
+}
+
+fn solve_part_two(id_ranges: &Vec<Vec<u64>>) -> u64 {
+    /* TODO options:
+    1. for each id in each range, get length. For n in 2..=length/2, check if length % n = 0.
+        if true, split id into n equal chunks. if all equal, total += id and break
+        NB: would .windows(length/n) work here?
+    2. make some crazy regex that checks for repeating digits and series of idigits until id.len()/2 for each id in each range.
+        if repeating digits -> total += id and break   */
+    return 64;
 }
