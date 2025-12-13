be580dbebd98d5058f6449a9b8d8239101938b3d
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-13 19:39:40 +0200

    Simplify part 1

--- year2025/src/days/day03.rs
+++ year2025/src/days/day03.rs
@@ -10,34 +10,37 @@ pub fn solve(file_path: &str) {
 
     let part_one_joltage: u32 = solve_part_one(&battery_banks);
     println!("Part one total joltage: {part_one_joltage}");
+
+    let part_two_joltage: u64 = solve_part_two(&battery_banks);
+    println!("Part two total joltage: {part_two_joltage}");
 }
 
 fn solve_part_one(battery_banks: &[Vec<u32>]) -> u32 {
     let mut total_joltage: u32 = 0;
     let battery_bank_length: usize = battery_banks[0].len();
     for battery_bank in battery_banks {
-        let (idx_max, max) = battery_bank.iter().enumerate().max_by_key(|(_idx, &val)| val).unwrap();
+        let max = battery_bank.iter().max().unwrap();
+        let idx_max = battery_bank.iter().position(|b|b == max).unwrap();
         if idx_max == battery_bank_length-1 {
             let remaining_batteries: &[u32] = &battery_bank[..idx_max];
             let remaining_batteries_max: &u32 = remaining_batteries.iter().max().unwrap();  
             let max_joltage: u32 = remaining_batteries_max * 10 + max;
             total_joltage += max_joltage;
-        } else if idx_max == 0 {
-            let remaining_batteries: &[u32] = &battery_bank[idx_max+1..];
-            let remaining_batteries_max: &u32 = remaining_batteries.iter().max().unwrap();  
-            let max_joltage = max * 10 + remaining_batteries_max;
-            total_joltage += max_joltage;
         } else {
-            let first_slice_batteries: &[u32] = &battery_bank[..idx_max];
-            let first_slice_max: &u32 = first_slice_batteries.iter().max().unwrap();
             let second_slice_batteries: &[u32] = &battery_bank[idx_max+1..];
             let second_slice_max: &u32 = second_slice_batteries.iter().max().unwrap();
-            if first_slice_max == max {
-                total_joltage += first_slice_max * 10 + max;
-            } else {
-                total_joltage += max * 10 + second_slice_max;
-            }
+            total_joltage += max * 10 + second_slice_max;
         }
+    }
+    total_joltage
+}
+
+fn solve_part_two(battery_banks: &[Vec<u32>]) -> u64 {
+    let mut total_joltage: u64 = 0;
+    for battery_bank in battery_banks {
+        let max = battery_bank.iter().max().unwrap();
+        let idx_max = battery_bank.iter().position(|b|b == max).unwrap();
+        total_joltage += 1;
     }
     total_joltage
 }
