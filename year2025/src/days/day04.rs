f5a5d2fb175e2af8aed4ad7786ebbb90b7904517
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-13 11:01:52 +0200

    Change for loop to .foreach and attempt to parallellise

--- year2025/src/days/day04.rs
+++ year2025/src/days/day04.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::num::NonZero;
 use grid2d::{Coordinate, Grid};
+// use rayon::prelude::*;
 
 pub fn solve(file_path: &str) {
     let input_file: String = fs::read_to_string(file_path).unwrap();
@@ -13,28 +14,30 @@ pub fn solve(file_path: &str) {
     let (accessible_paper_rolls, inaccessible_paper_rolls): (Vec<grid2d::Coordinate>, Vec<grid2d::Coordinate>) = accessible_paper_rolls(&printing_room_grid);
     println!("Accessible rolls: {}", accessible_paper_rolls.len());
 
-    let removable_rolls = solve_part_two(& mut printing_room_grid, &accessible_paper_rolls, &inaccessible_paper_rolls);
-    println!("Removable rolls: {removable_rolls}");
+    //let removable_rolls = solve_part_two(& mut printing_room_grid, &accessible_paper_rolls, &inaccessible_paper_rolls);
+    //println!("Removable rolls: {removable_rolls}");
 }
 
 fn accessible_paper_rolls(paper_room_grid: &Grid<char>) -> (Vec<grid2d::Coordinate>, Vec<grid2d::Coordinate>) {
     let mut accessible_paper_rolls: Vec<Coordinate>= vec![];
     let mut inaccessible_paper_rolls: Vec<Coordinate>= vec![];
-    for (coordinate, item) in paper_room_grid.enumerate() {
+    paper_room_grid.enumerate().for_each(|(coordinate, item)| {
         if *item == '@' && paper_room_grid.neighbors(coordinate).map(|i|i.1).filter(|c| **c == '@').count() < 4 {
             accessible_paper_rolls.push(coordinate);
         } else if *item == '@' {
             inaccessible_paper_rolls.push(coordinate);
         }
-    }
+    } );
     (accessible_paper_rolls, inaccessible_paper_rolls)
 }
 
 fn solve_part_two(paper_room_grid: & mut Grid<char>, accessible_rolls: &[Coordinate], inaccessible_rolls: &[Coordinate]) -> u32 {
+    println!("{paper_room_grid:?}");
     for coordinate in accessible_rolls {
         println!("{}", paper_room_grid.get(coordinate).unwrap());
-        let _ = paper_room_grid.get_mut(coordinate).insert(&mut '.');
+        paper_room_grid.get_mut(coordinate).map(|a|'.');
         println!("{}", paper_room_grid.get(coordinate).unwrap());
     }
+    println!("{paper_room_grid:?}");
     32
 }
