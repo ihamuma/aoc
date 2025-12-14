d2f6ea7afe1a76fbd9a7feecc4b8140470443b6c
Markus Ihamuotila <markusihamuotila@uprightproject.com>
2025-12-14 11:44:38 +0200

    Part Two ⭐

--- year2025/src/days/day03.rs
+++ year2025/src/days/day03.rs
@@ -5,7 +5,7 @@ pub fn solve(file_path: &str) {
     let battery_banks: Vec<Vec<u32>> = input_file
     .lines()
     .map(|bb|bb.chars()
-        .map(|b|b.to_digit(10).unwrap()).collect())
+        .map(|b: char|b.to_digit(10).unwrap()).collect())
     .collect();
 
     let part_one_joltage: u32 = solve_part_one(&battery_banks);
@@ -19,14 +19,14 @@ fn solve_part_one(battery_banks: &[Vec<u32>]) -> u32 {
     let mut total_joltage: u32 = 0;
     let battery_bank_length: usize = battery_banks[0].len();
     for battery_bank in battery_banks {
-        let max = battery_bank.iter().max().unwrap();
-        let idx_max = battery_bank.iter().position(|b|b == max).unwrap();
-        if idx_max == battery_bank_length-1 {
-            let remaining_batteries: &[u32] = &battery_bank[..idx_max];
+        let max: &u32 = battery_bank.iter().max().unwrap();
+        let low_bound: usize = battery_bank.iter().position(|b|b == max).unwrap();
+        if low_bound == battery_bank_length-1 {
+            let remaining_batteries: &[u32] = &battery_bank[..low_bound];
             let remaining_batteries_max: &u32 = remaining_batteries.iter().max().unwrap();  
             total_joltage += remaining_batteries_max * 10 + max;            
         } else {
-            let second_slice_batteries: &[u32] = &battery_bank[idx_max+1..];
+            let second_slice_batteries: &[u32] = &battery_bank[low_bound+1..];
             let second_slice_max: &u32 = second_slice_batteries.iter().max().unwrap();
             total_joltage += max * 10 + second_slice_max;
         }
@@ -35,23 +35,23 @@ fn solve_part_one(battery_banks: &[Vec<u32>]) -> u32 {
 }
 
 fn solve_part_two(battery_banks: &[Vec<u32>]) -> u64 {
-    let mut total_joltage: u64 = 0;
+    let mut all_joltages: Vec<u64> = vec![];
+    let battery_bank_length: usize = battery_banks[0].len();
     for battery_bank in battery_banks {
-        let mut joltages: Vec<u32> = vec![];
-        let mut idx_max: usize = 0;
-        let mut battery_bank_slice = &battery_bank[..];
+        let mut joltages: Vec<u64> = vec![];
+        let mut low_bound: usize = 0;
+        let mut high_bound: usize = battery_bank_length-12+joltages.len();
         while joltages.len() < 12 {
-            println!("Initially: {joltages:?} \n {idx_max} \n {battery_bank_slice:?}");
+            let battery_bank_slice: &[u32] = &battery_bank[low_bound..=high_bound];
             let max: &u32 = battery_bank_slice.iter().max().unwrap();
-            idx_max += battery_bank.iter().position(|b|b == max).unwrap();
-            if battery_bank_slice.len()-idx_max == 12-joltages.len() {
-                for b in battery_bank_slice { joltages.push(*b); }
-            } else {
-                joltages.push(*max);
-                battery_bank_slice = &battery_bank[idx_max + 1..];
-            }
+            joltages.push(*max as u64);
+            let idx_max_in_slice: usize = battery_bank_slice.iter().position(|b: &u32|b == max).unwrap();
+            low_bound += idx_max_in_slice + 1;
+            high_bound = battery_bank_length-12+joltages.len();
         }
-        total_joltage += 1;
+        let max_joltage: u64 = joltages.iter().fold(0, |acc: u64, elem: &u64| acc * 10 + elem);
+        all_joltages.push(max_joltage);
     }
-    total_joltage
+    let total_max_joltage: u64 = all_joltages.iter().sum();
+    total_max_joltage
 }
